6e4b57bae577f1187c421c0f20a97474395908d6
EnnyTheDonkey <Romek@Romek-Komputer>
2025-03-02 12:36:07 +0100

    Gold ring texture and item - to be edited! ; minor translation;

---
+++ src/main/java/miisterzmods/ringcraft/item/GoldRingItem.java
@@ -0,0 +1,26 @@
+
+package miisterzmods.ringcraft.item;
+
+import net.neoforged.api.distmarker.OnlyIn;
+import net.neoforged.api.distmarker.Dist;
+
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Item;
+import net.minecraft.network.chat.Component;
+
+import java.util.List;
+
+public class GoldRingItem extends Item {
+	public GoldRingItem() {
+		super(new Item.Properties().stacksTo(64).rarity(Rarity.UNCOMMON));
+	}
+
+	@Override
+	@OnlyIn(Dist.CLIENT)
+	public void appendHoverText(ItemStack itemstack, Item.TooltipContext context, List<Component> list, TooltipFlag flag) {
+		super.appendHoverText(itemstack, context, list, flag);
+		list.add(Component.translatable("item.ringcraft.gold_ring.description_0"));
+	}
+}
