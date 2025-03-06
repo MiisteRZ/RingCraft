dc9b7f45d68bc1618c565bd3a5c52297390b4c05
Tucky143 <110123789+Tucky143@users.noreply.github.com>
2025-03-05 18:02:36 -0700

    Add Blaze Soul

--- src/main/java/miisterzmods/ringcraft/init/RingcraftModItems.java
+++ src/main/java/miisterzmods/ringcraft/init/RingcraftModItems.java
@@ -16,6 +16,7 @@ import miisterzmods.ringcraft.item.ReinforcedRawIronItem;
 import miisterzmods.ringcraft.item.ReinforcedIronIngotItem;
 import miisterzmods.ringcraft.item.RegularRingItemItem;
 import miisterzmods.ringcraft.item.GoldRingItem;
+import miisterzmods.ringcraft.item.BlazeSoulItem;
 import miisterzmods.ringcraft.RingcraftMod;
 
 public class RingcraftModItems {
@@ -29,6 +30,7 @@ public class RingcraftModItems {
 	public static final DeferredItem<Item> RING_FORGE = block(RingcraftModBlocks.RING_FORGE);
 	public static final DeferredItem<Item> REINFORCED_IRON_DEEPSLATE_BLOCK = block(RingcraftModBlocks.REINFORCED_IRON_DEEPSLATE_BLOCK);
 	public static final DeferredItem<Item> REINFORCED_IRON_BLOCK = block(RingcraftModBlocks.REINFORCED_IRON_BLOCK);
+	public static final DeferredItem<Item> BLAZE_SOUL = REGISTRY.register("blaze_soul", BlazeSoulItem::new);
 
 	// Start of user code block custom items
 	// End of user code block custom items
