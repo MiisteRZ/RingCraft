80ad7aedc0e15b16ae028ff00fad2c1fd58fc44b
Tucky143 <110123789+Tucky143@users.noreply.github.com>
2025-03-15 19:35:05 -0600

    Fix

--- src/main/java/miisterzmods/ringcraft/network/Ring2BindingMessage.java
+++ src/main/java/miisterzmods/ringcraft/network/Ring2BindingMessage.java
@@ -1,13 +1,26 @@
 
 package miisterzmods.ringcraft.network;
 
+import net.neoforged.neoforge.network.handling.IPayloadContext;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.bus.api.SubscribeEvent;
+
+import net.minecraft.world.level.Level;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
+import net.minecraft.network.protocol.PacketFlow;
+import net.minecraft.network.codec.StreamCodec;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.RegistryFriendlyByteBuf;
+
+import miisterzmods.ringcraft.procedures.RingPower2KeybindOnKeyPressedProcedure;
 import miisterzmods.ringcraft.RingcraftMod;
 
 @EventBusSubscriber(bus = EventBusSubscriber.Bus.MOD)
 public record Ring2BindingMessage(int eventType, int pressedms) implements CustomPacketPayload {
-
 	public static final Type<Ring2BindingMessage> TYPE = new Type<>(ResourceLocation.fromNamespaceAndPath(RingcraftMod.MODID, "key_ring_2_binding"));
-
 	public static final StreamCodec<RegistryFriendlyByteBuf, Ring2BindingMessage> STREAM_CODEC = StreamCodec.of((RegistryFriendlyByteBuf buffer, Ring2BindingMessage message) -> {
 		buffer.writeInt(message.eventType);
 		buffer.writeInt(message.pressedms);
@@ -34,21 +47,17 @@ public record Ring2BindingMessage(int eventType, int pressedms) implements Custo
 		double x = entity.getX();
 		double y = entity.getY();
 		double z = entity.getZ();
-
 		// security measure to prevent arbitrary chunk generation
 		if (!world.hasChunkAt(entity.blockPosition()))
 			return;
-
 		if (type == 0) {
 
 			RingPower2KeybindOnKeyPressedProcedure.execute(world, x, y, z, entity);
 		}
-
 	}
 
 	@SubscribeEvent
 	public static void registerMessage(FMLCommonSetupEvent event) {
 		RingcraftMod.addNetworkMessage(Ring2BindingMessage.TYPE, Ring2BindingMessage.STREAM_CODEC, Ring2BindingMessage::handleData);
 	}
-
 }
