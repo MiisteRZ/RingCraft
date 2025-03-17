93bc0278668dd6c65f48cfb5422988f491c6dd19
MiisteRZ <cjimenez1161@gmail.com>
2025-03-17 23:52:17 +0100

    Minor Fixes

--- src/main/java/miisterzmods/ringcraft/procedures/IceKingEntityEntityIsHurtProcedure.java
+++ src/main/java/miisterzmods/ringcraft/procedures/IceKingEntityEntityIsHurtProcedure.java
@@ -8,8 +8,6 @@ import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.server.level.ServerLevel;
 
-import miisterzmods.ringcraft.RingcraftMod;
-
 public class IceKingEntityEntityIsHurtProcedure {
 	public static void execute(LevelAccessor world, double x, double y, double z, Entity entity) {
 		if (entity == null)
@@ -110,103 +108,5 @@ public class IceKingEntityEntityIsHurtProcedure {
 			_entityToSpawn.shoot(0, 1, (-1), (float) 0.5, 0);
 			projectileLevel.addFreshEntity(_entityToSpawn);
 		}
-		RingcraftMod.queueServerWork(10, () -> {
-			if (world instanceof ServerLevel projectileLevel) {
-				Projectile _entityToSpawn = new Object() {
-					public Projectile getProjectile(Level level, Entity shooter) {
-						Projectile entityToSpawn = new Snowball(EntityType.SNOWBALL, level);
-						entityToSpawn.setOwner(shooter);
-						return entityToSpawn;
-					}
-				}.getProjectile(projectileLevel, entity);
-				_entityToSpawn.setPos(x, (y + 3), z);
-				_entityToSpawn.shoot(1, 0, 0, (float) 0.5, 0);
-				projectileLevel.addFreshEntity(_entityToSpawn);
-			}
-			if (world instanceof ServerLevel projectileLevel) {
-				Projectile _entityToSpawn = new Object() {
-					public Projectile getProjectile(Level level, Entity shooter) {
-						Projectile entityToSpawn = new Snowball(EntityType.SNOWBALL, level);
-						entityToSpawn.setOwner(shooter);
-						return entityToSpawn;
-					}
-				}.getProjectile(projectileLevel, entity);
-				_entityToSpawn.setPos(x, (y + 3), z);
-				_entityToSpawn.shoot(0, 0, 1, (float) 0.5, 0);
-				projectileLevel.addFreshEntity(_entityToSpawn);
-			}
-			if (world instanceof ServerLevel projectileLevel) {
-				Projectile _entityToSpawn = new Object() {
-					public Projectile getProjectile(Level level, Entity shooter) {
-						Projectile entityToSpawn = new Snowball(EntityType.SNOWBALL, level);
-						entityToSpawn.setOwner(shooter);
-						return entityToSpawn;
-					}
-				}.getProjectile(projectileLevel, entity);
-				_entityToSpawn.setPos(x, (y + 3), z);
-				_entityToSpawn.shoot((-1), 0, 0, (float) 0.5, 0);
-				projectileLevel.addFreshEntity(_entityToSpawn);
-			}
-			if (world instanceof ServerLevel projectileLevel) {
-				Projectile _entityToSpawn = new Object() {
-					public Projectile getProjectile(Level level, Entity shooter) {
-						Projectile entityToSpawn = new Snowball(EntityType.SNOWBALL, level);
-						entityToSpawn.setOwner(shooter);
-						return entityToSpawn;
-					}
-				}.getProjectile(projectileLevel, entity);
-				_entityToSpawn.setPos(x, (y + 3), z);
-				_entityToSpawn.shoot(0, 0, (-1), (float) 0.5, 0);
-				projectileLevel.addFreshEntity(_entityToSpawn);
-			}
-			if (world instanceof ServerLevel projectileLevel) {
-				Projectile _entityToSpawn = new Object() {
-					public Projectile getProjectile(Level level, Entity shooter) {
-						Projectile entityToSpawn = new Snowball(EntityType.SNOWBALL, level);
-						entityToSpawn.setOwner(shooter);
-						return entityToSpawn;
-					}
-				}.getProjectile(projectileLevel, entity);
-				_entityToSpawn.setPos(x, (y + 3), z);
-				_entityToSpawn.shoot(1, 1, 0, (float) 0.5, 0);
-				projectileLevel.addFreshEntity(_entityToSpawn);
-			}
-			if (world instanceof ServerLevel projectileLevel) {
-				Projectile _entityToSpawn = new Object() {
-					public Projectile getProjectile(Level level, Entity shooter) {
-						Projectile entityToSpawn = new Snowball(EntityType.SNOWBALL, level);
-						entityToSpawn.setOwner(shooter);
-						return entityToSpawn;
-					}
-				}.getProjectile(projectileLevel, entity);
-				_entityToSpawn.setPos(x, (y + 3), z);
-				_entityToSpawn.shoot(0, 1, 1, (float) 0.5, 0);
-				projectileLevel.addFreshEntity(_entityToSpawn);
-			}
-			if (world instanceof ServerLevel projectileLevel) {
-				Projectile _entityToSpawn = new Object() {
-					public Projectile getProjectile(Level level, Entity shooter) {
-						Projectile entityToSpawn = new Snowball(EntityType.SNOWBALL, level);
-						entityToSpawn.setOwner(shooter);
-						return entityToSpawn;
-					}
-				}.getProjectile(projectileLevel, entity);
-				_entityToSpawn.setPos(x, (y + 3), z);
-				_entityToSpawn.shoot((-1), 1, 0, (float) 0.5, 0);
-				projectileLevel.addFreshEntity(_entityToSpawn);
-			}
-			if (world instanceof ServerLevel projectileLevel) {
-				Projectile _entityToSpawn = new Object() {
-					public Projectile getProjectile(Level level, Entity shooter) {
-						Projectile entityToSpawn = new Snowball(EntityType.SNOWBALL, level);
-						entityToSpawn.setOwner(shooter);
-						return entityToSpawn;
-					}
-				}.getProjectile(projectileLevel, entity);
-				_entityToSpawn.setPos(x, (y + 3), z);
-				_entityToSpawn.shoot(0, 1, (-1), (float) 0.5, 0);
-				projectileLevel.addFreshEntity(_entityToSpawn);
-			}
-		});
 	}
 }
